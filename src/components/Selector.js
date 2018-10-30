10e7938f54619d8699f7d50a2e8bf04fe601fc61
David <encore@berkeley.edu>
2018-10-30 16:24:50 -0700

    First Commit

---
+++ src/components/Selector.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <div className="red-bg selector">
+        <div className="text">Project</div>
+      </div>
+      <div className="blue-bg selector">
+        <div className="text">Github</div>
+      </div>
+      <div className="orange-bg selector">
+        <div className="text">Resume</div>
+      </div>
+    </div>
+  );
+};
