ef622cc8e665f5f41fe7fc9da607c4f820d8ee3e
dawei <imdavidf@gmail.com>
2018-11-01 14:34:16 -0700

    Create project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import Project from './components/Project';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route path="/" exact component={Home} />
+        <Route path="/project" exact component={Project} />
       </div>
     );
   }
---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+
+    render() {
+        return (
+            <div>
+                Project
+            </div>
+        );
+    }
+};
+
+export default Project;
