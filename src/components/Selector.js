ae02300157610741c0c64b990072cec5034ab154
dawei <imdavidf@gmail.com>
2018-11-01 18:29:08 -0700

    Fix Mobile View issues

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -7,7 +7,7 @@ export default class Selector extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {iteration: 0, targets: [], hoveringEle: null}
+    this.state = {iteration: 0, targets: [], hoveringEle: null, mainTarget: null}
   }
   
   handleOnAnimationEnd = (e) => {
@@ -36,31 +36,45 @@ export default class Selector extends Component {
     } else {
       // Single Mode
       if (this.props.name === target.id ) {
-        let {targets} = this.state;
-        targets = targets.concat([target]);
-        targets.forEach((target) => {
-          target.children[0].classList = ""
-          target.classList.add("selector-expand")
-          target.children[0].classList.add("text-expand")
-        })
+        this.setState({mainTarget: target});
+        target.children[0].classList = "";
+        target.classList.add("selector-expand");
+        target.children[0].classList.add("text-expand");
       }
     }
   }
 
-  handleMouseEnter = (e) => {
+  handleMouseOver = (e) => {
+    if (this.props.expand) {
+      return;
+    }
     e.stopPropagation()
     let {target} = e;
     // Make sure the mouse is on the parent of the node
     if (target.id) {
-      if (target.id === this.props.name) {
-        return;
-      }
       if (this.state.iteration>2 & target !== undefined) {
-        this.setState({hoveringEle: target})
-        target.classList.add("selector-expand")
         
+        // Collapse the main seletor
+        const mainTarget = this.state.mainTarget;
+        mainTarget.classList.remove("selector-expand")
+        mainTarget.children[0].classList = "";
+        mainTarget.children[0].classList.add("text");
+        
+        // Fix extra-expanding problem in mobile view
+        let {targets} = this.state;
+        targets.forEach((t) => {
+          t.classList.remove("selector-expand");
+          t.children[0].classList = "";
+          t.children[0].classList.add("text");
+        })
+        targets = [target];
+
+        // Expand the hovering selector
+        this.setState({hoveringEle: target, targets})
+        target.classList.add("selector-expand")
         target.children[0].classList = ""
         target.children[0].classList.add("text-expand")
+        
       }
     }
     
@@ -76,9 +90,15 @@ export default class Selector extends Component {
     }
     if (this.state.iteration>2 & target !== null) {
       let target = this.state.hoveringEle;
+      const mainTarget = this.state.mainTarget;
       target.classList.remove("selector-expand");
       target.children[0].classList = "";
       target.children[0].classList.add("text");
+
+      // Recover main target
+      mainTarget.classList.add("selector-expand")
+      mainTarget.children[0].classList = ""
+      mainTarget.children[0].classList.add("text-expand")
     }
   }
 
@@ -86,21 +106,21 @@ export default class Selector extends Component {
     return (
       <div className="" onAnimationEnd={this.handleOnAnimationEnd}>
         <Link to="/project">
-          <div className="red-bg selector animated bounce" id="Project" onMouseEnter={this.props.expand ? null : this.handleMouseEnter} onMouseLeave={this.props.expand ? null :this.handleMouseLeave}>
+          <div className="red-bg selector animated bounce" id="Project" onMouseOver={this.props.expand ? null : this.handleMouseOver} onMouseLeave={this.props.expand ? null :this.handleMouseLeave}>
             <div className="text">
               Project
             </div>
           </div>
         </Link>
         <a href="https://github.com/daweifeng">
-          <div className="blue-bg selector animated bounce delay-025" id="Github" onMouseEnter={this.props.expand ? null : this.handleMouseEnter} onMouseLeave={this.props.expand ? null :this.handleMouseLeave}>
+          <div className="blue-bg selector animated bounce delay-025" id="Github" onMouseOver={this.props.expand ? null : this.handleMouseOver} onMouseLeave={this.props.expand ? null :this.handleMouseLeave}>
             <div className="text">
               Github
             </div>
           </div>
         </a>
         <a href="https://s3-us-west-2.amazonaws.com/daweifeng/public/Dawei_Feng_Resume.pdf">
-        <div className="orange-bg selector animated bounce delay-05" id="Resume" onMouseEnter={this.props.expand ? null : this.handleMouseEnter} onMouseLeave={this.props.expand ? null :this.handleMouseLeave}>
+        <div className="orange-bg selector animated bounce delay-05" id="Resume" onMouseOver={this.props.expand ? null : this.handleMouseOver} onMouseLeave={this.props.expand ? null :this.handleMouseLeave}>
           <div className="text">            
               Resume
           </div>
