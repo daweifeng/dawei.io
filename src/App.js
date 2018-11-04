2d341b62daad11c11b8fea4f8baac3e7753e0ce7
David <encore@berkeley.edu>
2018-11-03 18:54:53 -0700

    Move to react-ga

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import ReactGA from "react-ga";
+import withTracker from "./components/withTracker";
 import "./App.css";
 import Home from "./components/Home";
 import Project from "./components/Project";
 
-ReactGA.initialize("UA-128591856-1");
-
 class App extends Component {
-  fireGA = nextState => {
-    const { pathName } = nextState.location;
-    ReactGA.pageview(pathName);
-  };
-
   render() {
     return (
       <div className="App">
-        <Route path="/" exact onEnter={this.fireGA} component={Home} />
-        <Route
-          path="/project"
-          exact
-          onEnter={this.fireGA}
-          component={Project}
-        />
+        <Route path="/" exact component={withTracker(Home)} />
+        <Route path="/project" exact component={withTracker(Project)} />
       </div>
     );
   }
