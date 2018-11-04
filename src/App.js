83bc9b0e1ca67e22aebacfb49236ee9416bec16d
David <encore@berkeley.edu>
2018-11-03 18:29:11 -0700

    Add Google Analysis

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import ReactGA from "react-ga";
 import "./App.css";
 import Home from "./components/Home";
 import Project from "./components/Project";
 
+ReactGA.initialize("UA-128591856-1");
+
 class App extends Component {
+  fireGA = nextState => {
+    const { pathName } = nextState.location;
+    ReactGA.pageview(pathName);
+  };
+
   render() {
     return (
       <div className="App">
-        <Route path="/" exact component={Home} />
-        <Route path="/project" exact component={Project} />
+        <Route path="/" exact onEnter={this.fireGA} component={Home} />
+        <Route
+          path="/project"
+          exact
+          onEnter={this.fireGA}
+          component={Project}
+        />
       </div>
     );
   }
