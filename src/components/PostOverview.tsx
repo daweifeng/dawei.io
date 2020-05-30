131b3672d1bed284d9f75e61a7d18f56e1ac0191
Dawei Feng <imdavidf@gmail.com>
2020-05-29 19:46:28 -0700

    Intergrate ghost

--- src/components/PostOverview.tsx
+++ src/components/PostOverview.tsx
@@ -1,23 +1,35 @@
 import React from "react";
+import moment from "moment";
 
 import "../styles/post.css";
 
-export default  (): JSX.Element => {
+export interface Props{
+  data: any,
+  key: string
+}
+
+export default  (props: Props): JSX.Element => {
 
   return (
     <div className="overview">
-      <div className="img" style={{backgroundImage: `url(${"https://cdn.worldvectorlogo.com/logos/redis.svg"})`}}>
+      <div className="img" style={{backgroundImage: `url(${props.data.feature_image})`}}>
       </div>
       <div className="content">
-        <div className="title">A delayed SMS system</div>
+      <div className="title">{props.data.title}</div>
         <div className="info">
           <div className="tags">
-            <span>Backend</span>
+            {renderTags(props.data.tags)}
           </div>
 
-          <div className="time">01/20/2020</div>
+    <div className="time">{moment(props.data.updated_at).format("LL")}</div>
+          </div>
         </div>
-      </div>
     </div>
   );
+};
+
+const renderTags = (tags: any) => {
+  return tags.map((tag: any, index: any) => (
+    <span key={`tag_${tag.name}_{${index}}`}>{tag.name}</span>
+  ));
 };
